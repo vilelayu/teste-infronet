57d7e9150b8ae3f0509ebd3e9c863608dd7c2e7d
Yuri Souza <yurivilela@live.com>
2026-01-21 08:46:02 -0300

    implementacao  de tratativa de erro no metodo de pesquisa

--- teste/app/Http/Controllers/TasksController.php
+++ teste/app/Http/Controllers/TasksController.php
@@ -2,15 +2,28 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Tasks;
+use Illuminate\Support\Facades\Log;
 
 class TasksController extends Controller
 {
     public function index()
     {
-     $list = Tasks::all();
-     return $list->isEmpty()
-        ? response()->noContent()
-        : response()->json($list);
+        try{
+
+            $list = Tasks::all();
+            return $list->isEmpty()
+                ? response()->noContent()
+                : response()->json($list);
+        }
+        catch(\Exception $e){
+            Log::error('Erro ao consultar a lista de task', [
+                'error' => $e->getMessage()
+            ]);
+
+            return response()->json([
+                'message' => 'Erro ao consultar o registro'
+            ], 400);
+        }
     }
     public function store(Request $request)
     {
@@ -22,24 +35,10 @@ class TasksController extends Controller
         return response()->json($task, 201);
     }
 
-    public function show($id)
-    {
-        return response()->json([
-            'message' => "Detalhe do Faz Cruz {$id}"
-        ]);
-    }
-
     public function update(Request $request, $id)
     {
         return response()->json([
             'message' => "Faz Cruz {$id} atualizado"
-        ]);
-    }
-
-    public function destroy($id)
-    {
-        return response()->json([
-            'message' => "Faz Cruz {$id} removido"
         ]);
     }
 }
--- teste/routes/web.php
+++ teste/routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TasksController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::apiResource('api/task',TasksController::class);
